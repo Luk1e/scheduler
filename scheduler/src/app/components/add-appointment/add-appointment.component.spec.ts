ac485f65a1ee2e2eb880c9b6f44463121a6a59f7
Luk1e <luka.gogiashvili.02@gmail.com>
2024-09-13 13:41:50 +0400

    Update: changed styles

--- scheduler/src/app/components/add-appointment/add-appointment.component.spec.ts
+++ scheduler/src/app/components/add-appointment/add-appointment.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AddAppointmentComponent } from './add-appointment.component';
 
 describe('AddAppointmentComponent', () => {
